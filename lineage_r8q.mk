7a9ecd970ad956232ecd30d49767b2da7427656a
Atakan <ata.kaner03@gmail.com>
2025-04-13 17:03:13 +0300

    r8q: Bump build description to Tiramisu
    Change-Id: I50e9c82a05e5cc89a19119cbdd1a3e2284f7b531

--- lineage_r8q.mk
+++ lineage_r8q.mk
@@ -40,7 +40,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="r8qxx-user 11 RP1A.200720.012 G780GXXSDEXJ3 release-keys" \
+    BuildDesc="r8qxx-user 13 TP1A.220624.014 G780GXXSDEXJ3 release-keys" \
     BuildFingerprint=samsung/r8qxx/r8q:11/RP1A.200720.012/G780GXXSDEXJ3:user/release-keys \
     DeviceProduct=r8qxx \
     SystemName=r8qxx
